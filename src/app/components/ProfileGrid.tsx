1e096cd7b441cfcb5fde142c5ed83596afa348a7
osarodev <osernon@gmail.com>
2025-06-15 19:53:06 +0100

    adjust homepage image dimensions

--- src/app/components/ProfileGrid.tsx
+++ src/app/components/ProfileGrid.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 const ProfileGrid = () => {
   return (
     <div>
-      <div className='w-full h-[215px] p-5'>
+      <div className='w-full h-[300px] p-5'>
         <Image
           src={
             'https://res.cloudinary.com/dlahyjhur/image/upload/v1747656124/pointing-at-laptop_k74oyb.jpg'
           }
           alt={'coding'}
           width={800}
-          height={215}
+          height={300}
           className='w-full h-full object-cover rounded-tr-4xl  rounded-bl-4xl'
         />
       </div>
